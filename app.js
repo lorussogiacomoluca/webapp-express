0ae2148b217cfb2f9ab8a8dbd10421999b1292fd
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-29 17:21:49 +0200

    [UPD] notFound Middleware

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//Middlewares
+//notFound.js
+const notFound = require("./middlewares/notFound.js");
+
+//public
+app.use(express.static("public"));
+
 //DB
 const connection = require("./data/db");
 
@@ -13,9 +20,14 @@ const movieRouter = require("./routers/movieRouter.js");
 app.use("/api/movies", movieRouter);
 
 //Entry point
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome to my movies library");
 });
+
+/* -------------------------------------------------------------------------- */
+/*                                 MiddleWares                                */
+/* -------------------------------------------------------------------------- */
+app.use(notFound);
 
 //Listen
 app.listen(port, () => {
