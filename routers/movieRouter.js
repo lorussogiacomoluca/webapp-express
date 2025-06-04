591b81230dc641d839073aceb330706bffd5d075
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-06-04 17:12:10 +0200

    [UPD] Create route

--- routers/movieRouter.js
+++ routers/movieRouter.js
@@ -4,5 +4,7 @@ const movieController = require("../controllers/movieController.js");
 
 router.get("/", movieController.index);
 router.get("/:id", movieController.show);
+//Route for Post Review
+router.post('/"id/review', movieController.storeReview);
 
 module.exports = router;
